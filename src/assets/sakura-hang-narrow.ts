5052157b168e1c279ca9ec4d4deeb79ddba584b3
Stephen Horton <54323940+StephenSHorton@users.noreply.github.com>
2026-09-21 15:43:38 -0600

    Point the narrow sakura painting at eight short-line modules.

--- src/assets/sakura-hang-narrow.ts
+++ src/assets/sakura-hang-narrow.ts
@@ -1,4 +1,4 @@
-// Original hanging-sakura painting (3:4, narrow). Enters from the top-right.
+// Original hanging-sakura painting. Not a photo, not SVG sticks.
 import { part as p0 } from './sakura-hang-narrow-p0'
 import { part as p1 } from './sakura-hang-narrow-p1'
 import { part as p2 } from './sakura-hang-narrow-p2'
@@ -7,7 +7,5 @@ import { part as p4 } from './sakura-hang-narrow-p4'
 import { part as p5 } from './sakura-hang-narrow-p5'
 import { part as p6 } from './sakura-hang-narrow-p6'
 import { part as p7 } from './sakura-hang-narrow-p7'
-import { part as p8 } from './sakura-hang-narrow-p8'
-import { part as p9 } from './sakura-hang-narrow-p9'
-const src = 'data:image/jpeg;base64,' + p0 + p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9
+const src = 'data:image/jpeg;base64,' + p0 + p1 + p2 + p3 + p4 + p5 + p6 + p7
 export default src
