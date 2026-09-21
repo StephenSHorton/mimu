756f16c37b865abbd375fc48fbd4ef90de148e0e
Stephen Horton <54323940+StephenSHorton@users.noreply.github.com>
2026-09-21 16:58:39 -0600

    Restore sakura-hang assembler to 8-import p0-p7

--- src/assets/sakura-hang.ts
+++ src/assets/sakura-hang.ts
@@ -5,9 +5,7 @@ import { part as p2 } from './sakura-hang-p2'
 import { part as p3 } from './sakura-hang-p3'
 import { part as p4 } from './sakura-hang-p4'
 import { part as p5 } from './sakura-hang-p5'
-import { part as p6a } from './sakura-hang-p6a'
-import { part as p6b } from './sakura-hang-p6b'
-import { part as p7a } from './sakura-hang-p7a'
-import { part as p7b } from './sakura-hang-p7b'
-const src = 'data:image/jpeg;base64,' + p0 + p1 + p2 + p3 + p4 + p5 + p6a + p6b + p7a + p7b
+import { part as p6 } from './sakura-hang-p6'
+import { part as p7 } from './sakura-hang-p7'
+const src = 'data:image/jpeg;base64,' + p0 + p1 + p2 + p3 + p4 + p5 + p6 + p7
 export default src
