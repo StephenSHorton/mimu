26865f286a1e1a02bffe763f8df18e7ba4b6285b
Stephen Horton <54323940+StephenSHorton@users.noreply.github.com>
2026-09-21 16:07:47 -0600

    Fix hang-p7b and add narrow assembler plus p0 split modules

--- src/assets/sakura-hang-narrow.ts
+++ src/assets/sakura-hang-narrow.ts
@@ -1,11 +1,19 @@
 // Original hanging-sakura painting. Not a photo, not SVG sticks.
-import { part as p0 } from './sakura-hang-narrow-p0'
-import { part as p1 } from './sakura-hang-narrow-p1'
-import { part as p2 } from './sakura-hang-narrow-p2'
-import { part as p3 } from './sakura-hang-narrow-p3'
-import { part as p4 } from './sakura-hang-narrow-p4'
-import { part as p5 } from './sakura-hang-narrow-p5'
-import { part as p6 } from './sakura-hang-narrow-p6'
-import { part as p7 } from './sakura-hang-narrow-p7'
-const src = 'data:image/jpeg;base64,' + p0 + p1 + p2 + p3 + p4 + p5 + p6 + p7
+import { part as p0a } from './sakura-hang-narrow-p0a'
+import { part as p0b } from './sakura-hang-narrow-p0b'
+import { part as p1a } from './sakura-hang-narrow-p1a'
+import { part as p1b } from './sakura-hang-narrow-p1b'
+import { part as p2a } from './sakura-hang-narrow-p2a'
+import { part as p2b } from './sakura-hang-narrow-p2b'
+import { part as p3a } from './sakura-hang-narrow-p3a'
+import { part as p3b } from './sakura-hang-narrow-p3b'
+import { part as p4a } from './sakura-hang-narrow-p4a'
+import { part as p4b } from './sakura-hang-narrow-p4b'
+import { part as p5a } from './sakura-hang-narrow-p5a'
+import { part as p5b } from './sakura-hang-narrow-p5b'
+import { part as p6a } from './sakura-hang-narrow-p6a'
+import { part as p6b } from './sakura-hang-narrow-p6b'
+import { part as p7a } from './sakura-hang-narrow-p7a'
+import { part as p7b } from './sakura-hang-narrow-p7b'
+const src = 'data:image/jpeg;base64,' + p0a + p0b + p1a + p1b + p2a + p2b + p3a + p3b + p4a + p4b + p5a + p5b + p6a + p6b + p7a + p7b
 export default src
